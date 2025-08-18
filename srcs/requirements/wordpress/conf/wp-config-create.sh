847cdfc109b6fd47eedb77033527caa498481f16
Kay Freyer <kfreyer@inception.42wolfsburg.de>
2025-08-18 13:52:06 +0200

    delete the css locations in nginx.conf

--- srcs/requirements/wordpress/conf/wp-config-create.sh
+++ srcs/requirements/wordpress/conf/wp-config-create.sh
@@ -3,22 +3,17 @@
 if [ ! -f "/var/www/wp-config.php" ]; then
     cat <<EOF >/var/www/wp-config.php
 <?php
-define( 'DB_NAME', '${DB_NAME}' );
+define( 'DB_NAME', '${DB_NAME}' ); # PHP function creating constant
 define( 'DB_USER', '${DB_USER}' );
 define( 'DB_PASSWORD', '${DB_PASS}' );
 define( 'DB_HOST', 'mariadb' );
 define( 'DB_CHARSET', 'utf8' );
-define( 'DB_COLLATE', '' );
+define( 'DB_COLLATE', '' ); # Empty = use database default
 define('FS_METHOD','direct');
-\$table_prefix = 'wp_';
+\$table_prefix = 'wp_';         # wordpress expects this specific variable name
 define( 'WP_DEBUG', false );
 if ( ! defined( 'ABSPATH' ) ) {
 define( 'ABSPATH', __DIR__ . '/' );}
-define( 'WP_REDIS_HOST', 'redis' );
-define( 'WP_REDIS_PORT', 6379 );
-define( 'WP_REDIS_TIMEOUT', 1 );
-define( 'WP_REDIS_READ_TIMEOUT', 1 );
-define( 'WP_REDIS_DATABASE', 0 );
 require_once ABSPATH . 'wp-settings.php';
 EOF
 fi
